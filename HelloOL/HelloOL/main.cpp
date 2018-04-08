c4b2f09e1168c18f7b5be8aa629ce9bd040dcc67
Stone <827443469@qq.com>
2018-04-08 23:42:17 +0800

    创建窗口

--- HelloOL/HelloOL/main.cpp
+++ HelloOL/HelloOL/main.cpp
@@ -1,19 +1,34 @@
-#include <GL\glew.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
 
-
-#include "display.h"
 
 
 int main(int argc, char** argv)
 {
-	Display display(400, 300, "hello world!");
-
-	while (!display.IsClosed())
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		display.Clear(0.0f, 1.0f, 0.0f, 1.0f);
-
-		display.Update();// Ë¢ÐÂ
+		std::cout << "Faild to initialize GLAD" << std::endl;
+		return -1;
 	}
 
-	return 0;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "learn_open_gl", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "faild to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glViewport(0, 0, 800, 600);
+	while (true)
+	{
+
+	}
 }
