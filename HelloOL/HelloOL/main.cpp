003d07c2cd51fb4197666243a9bcad612427c8fd
Stone <xiezf001@qq.com>
2018-04-10 19:16:31 +0800

    make windowa and process key input

--- HelloOL/HelloOL/main.cpp
+++ HelloOL/HelloOL/main.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 using namespace std;
 
-
+void processInput(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main(int argc, char** argv)
 {
-
+	/*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "faild to initalize Glad" << std::endl;
+		return -1;
+	}*/
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -21,8 +26,30 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	while (true)
+	//glViewport(0, 0, 800, 600);
+	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	while (!glfwWindowShouldClose(window))
 	{
-
+		processInput(window);
+		/*glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);*/
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
+	glfwTerminate();
+	return 0;
 }
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
