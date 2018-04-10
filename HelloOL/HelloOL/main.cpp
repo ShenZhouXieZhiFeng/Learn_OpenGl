b486f77b9fb89b83094f53e11bbae0ee269daf80
Stone <827443469@qq.com>
2018-04-10 23:31:02 +0800

    change code and show viewport and clear color

--- HelloOL/HelloOL/main.cpp
+++ HelloOL/HelloOL/main.cpp
@@ -8,11 +8,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main(int argc, char** argv)
 {
-	/*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "faild to initalize Glad" << std::endl;
-		return -1;
-	}*/
+	
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -26,15 +22,19 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	//glViewport(0, 0, 800, 600);
-	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	//glClear(GL_COLOR_BUFFER_BIT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
-		/*glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);*/
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
